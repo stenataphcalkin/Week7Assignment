68b629bdfef8e4c735ef7a01c1d1fb374ec8fcdc
Steph Kincarr <steph@polyhydral.co.uk>
2025-09-26 13:53:27 +0100

    chore:Pre-setup push for Render

---
+++ server/server.js
@@ -0,0 +1,8 @@
+//set up a server
+
+//===================================================
+// TODO: I want a route to READ data (you can have more than one)
+// TODO: I want a route to CREATE data
+
+//? Stretch: I want a route to DELETE data
+//? Stretch: I want a route to UPDATE data
