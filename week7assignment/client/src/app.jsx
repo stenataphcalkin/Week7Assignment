6e1c2c1ea362256bd889d3f831bd0e0cea25da39
Steph Kincarr <steph@polyhydral.co.uk>
2025-09-26 15:25:36 +0100

    chore:groundwork and structuring. Initial image added for img library

---
+++ week7assignment/client/src/app.jsx
@@ -0,0 +1,17 @@
+import "./app.css";
+
+// Header
+import Header from "./components/header/header.jsx";
+import "./components/header/header.css";
+
+//Form and Image Container
+import FormImageContainer from "./components/formImageContainer/formImageContainer.jsx";
+import "./components/formImageContainer/formImageContainer.css";
+
+//Reviews Container
+import ReviewsContainer from "./components/reviewsContainer/reviewsContainer.jsx";
+import "./components/reviewsContainer/reviewsContainer.css";
+
+// Footer
+import Footer from "./components/footer/footer.jsx";
+import "./components/footer/footer.css";
